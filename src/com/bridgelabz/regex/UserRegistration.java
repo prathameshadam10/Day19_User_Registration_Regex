901b7be04beacc01010db1c92bdc803e479da695
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-03-30 00:12:55 +0530

    Update UserRegistration.java

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -5,7 +5,13 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("!!!!!!!USER REGISTRATION PROGRAM!!!!!!");
+        System.out.println("!!!!!!USER REGISTRATION!!!!!!!");
+//        UC1 First Name
+        Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{2,}");
+        Matcher matcher1 = pattern1.matcher("Prathamesh");
+        if (matcher1.matches())
+            System.out.println("valid");
+        else
+            System.out.println("Invalid");
     }
-
 }
